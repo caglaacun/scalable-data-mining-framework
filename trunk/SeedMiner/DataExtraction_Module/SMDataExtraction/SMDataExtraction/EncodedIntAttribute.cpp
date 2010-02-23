65306f72e0ffb344f3712fea9dce9e2de58d03a2
samfernandopulle <samfernandopulle@c7f6ba40-6498-11de-987a-95e5a5a5d5f1>
2010-02-23 19:22:06 +0000

    Commit the changes made.

--- trunk/SeedMiner/DataExtraction_Module/SMDataExtraction/SMDataExtraction/EncodedIntAttribute.cpp
+++ trunk/SeedMiner/DataExtraction_Module/SMDataExtraction/SMDataExtraction/EncodedIntAttribute.cpp
@@ -12,7 +12,7 @@ EncodedIntAttribute::~EncodedIntAttribute(void)
 {
 }
 
-vector<VBitStream> EncodedIntAttribute::vBitStreams(){
+vector<VBitStream*> EncodedIntAttribute::vBitStreams(){
 	return this->_vBitStreamSet;
 }
 
@@ -20,7 +20,7 @@ vector<bool> EncodedIntAttribute::SignBitMap(){
 	return this->_signBitMap;
 }
 
-char* EncodedIntAttribute::attributeName(){
+string EncodedIntAttribute::attributeName(){
 	return this->_attName;
 }
 
@@ -32,16 +32,21 @@ ATT_TYPE EncodedIntAttribute::attributeType(){
 	return this->_attType;
 }
 
-VBitStream EncodedIntAttribute::bitStreamAt(int bitStreamID){
-	int temp = 0;
+void EncodedIntAttribute::setVBitStreamSize(int size){
+	this->_vBitStreamSet.resize(size);
+}
+
+VBitStream* EncodedIntAttribute::bitStreamAt(int bitStreamID){
+	/*int temp = 0;
 	for (; temp<this->_vBitStreamSet.size() ; temp++)
 	{
-		if (this->_vBitStreamSet[temp].bitStreamPosition() == bitStreamID)
+		if (this->_vBitStreamSet[temp]->bitStreamPosition() == bitStreamID)
 		{
 			return this->_vBitStreamSet[temp];
 		}
 	}
-	return NULL;
+	return NULL;*/
+	return this->_vBitStreamSet[bitStreamID];
 }
 
 int EncodedIntAttribute::getTheSignOf(int tupleID){
@@ -54,7 +59,7 @@ int EncodedIntAttribute::decodeTheTuple(int tupleID){
 	int val=0;
 	for (; i<this->_noOfVBitStreams ; i++)
 	{
-		bitSet[i] = this->_vBitStreamSet[i].BitStream()[tupleID - 1];
+		bitSet[i] = this->_vBitStreamSet[i]->BitStream()[tupleID - 1];
 		if(bitSet[i]) {val += pow(2.0,(double)(this->_noOfVBitStreams - i - 1));}
 	}
 
@@ -66,7 +71,7 @@ int EncodedIntAttribute::decodeTheTuple(int tupleID){
 	return val;
 }
 
-void EncodedIntAttribute::setAttName(char* name){
+void EncodedIntAttribute::setAttName(string name){
 	this->_attName = name;
 }
 
@@ -74,14 +79,25 @@ void EncodedIntAttribute::setAttType(ATT_TYPE type){
 	this->_attType = type;
 }
 
-void EncodedIntAttribute::setNoOfVBitStreams(int novBitsets){
+void EncodedIntAttribute::setNoOfVBitStreams(int novBitsets,int noRows){
 	this->_noOfVBitStreams = novBitsets;
+	this->_vBitStreamSet.resize(this->_noOfVBitStreams);
+
+	for (int i = 0 ; i <novBitsets ; i++)
+	{
+		this->_vBitStreamSet[i] = new VBitStream(noRows);
+	}
 }
 
-void EncodedIntAttribute::setVBitStreams(vector<VBitStream> vBitset){
+void EncodedIntAttribute::setVBitStreams(vector<VBitStream*> vBitset){
 	this->_vBitStreamSet = vBitset;
 }
 
-void EncodedIntAttribute::setSignBitMap(vector<bool> sBitMap){
-	this->_signBitMap = sBitMap;
+void EncodedIntAttribute::setTheSignBitMap(long int *values,int valSet){
+	this->_signBitMap.resize(valSet);
+	for (int i = 0 ; i < valSet ; i++)
+	{
+		if(values[i] == abs(values[i])) this->_signBitMap[i] = false;
+		else this->_signBitMap[i] = true;
+	}
 }
--- trunk/SeedMiner/DataExtraction_Module/SMDataExtraction/SMDataExtraction/EncodedIntAttribute.h
+++ trunk/SeedMiner/DataExtraction_Module/SMDataExtraction/SMDataExtraction/EncodedIntAttribute.h
@@ -10,24 +10,25 @@ class EncodedIntAttribute
 public:
 	__declspec(dllexport) EncodedIntAttribute(void);
 	__declspec(dllexport) ~EncodedIntAttribute(void);
-	__declspec(dllexport) vector<VBitStream> vBitStreams();
+	__declspec(dllexport) vector<VBitStream*> vBitStreams();
 	__declspec(dllexport) vector<bool> SignBitMap();
-	__declspec(dllexport) char* attributeName();
+	__declspec(dllexport) string attributeName();
 	__declspec(dllexport) int NoOfVBitStreams();
 	__declspec(dllexport) ATT_TYPE attributeType();
-	__declspec(dllexport) void setVBitStreams(vector<VBitStream> vBitset);
-	__declspec(dllexport) void setSignBitMap(vector<bool> sBitMap);
-	__declspec(dllexport) void setNoOfVBitStreams(int novBitsets);
+	__declspec(dllexport) void setVBitStreams(vector<VBitStream*> vBitset);
+	__declspec(dllexport) void setNoOfVBitStreams(int novBitsets,int rows);
 	__declspec(dllexport) void setAttType(ATT_TYPE type);
-	__declspec(dllexport) void setAttName(char* name);
-	__declspec(dllexport) VBitStream bitStreamAt(int bitStreamID);
+	__declspec(dllexport) void setAttName(string name);
+	__declspec(dllexport) VBitStream* bitStreamAt(int bitStreamID);
 	__declspec(dllexport) int decodeTheTuple(int tupleID);
 	__declspec(dllexport) int getTheSignOf(int tupleID);
+	__declspec(dllexport) void setTheSignBitMap(long int *values,int valSet);
+	__declspec(dllexport) void setVBitStreamSize(int size);
 
 private:
-	vector<VBitStream> _vBitStreamSet;
+	vector<VBitStream*> _vBitStreamSet;
 	vector<bool> _signBitMap; //sign bit map 0 for + and 1 for -.
 	ATT_TYPE _attType;
-	char* _attName;
+	string _attName;
 	int _noOfVBitStreams;
 };
