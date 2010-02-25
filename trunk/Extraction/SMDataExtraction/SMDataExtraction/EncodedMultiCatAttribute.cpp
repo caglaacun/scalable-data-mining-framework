e6d0f7de718084b7aec6183205c86f331d0f33b5
samfernandopulle <samfernandopulle@c7f6ba40-6498-11de-987a-95e5a5a5d5f1>
2010-02-25 06:52:43 +0000

    change the design with applying template design pattern on it.

--- trunk/Extraction/SMDataExtraction/SMDataExtraction/EncodedMultiCatAttribute.cpp
+++ trunk/Extraction/SMDataExtraction/SMDataExtraction/EncodedMultiCatAttribute.cpp
@@ -23,22 +23,23 @@ int EncodedMultiCatAttribute::noOfUniqueValues(){
 void EncodedMultiCatAttribute::mapStringDataToCategories(string* _valueList,vector<string> _uniqueValList,int noOfRows){
 	this->_uniqueValList = _uniqueValList;
 	int maxUniqueIndex = _uniqueValList.size();
-	this->_noOfVBitStreams = (int)(ceil(log10((double)maxUniqueIndex)/log10(2.0)));
-	this->_vBitStreamSet.resize(this->_noOfVBitStreams);
+	int temp = (int)(ceil(log10((double)maxUniqueIndex)/log10(2.0)));
+	this->setNoOfVBitStreams(temp,noOfRows);
+	this->setVBitStreamSize(this->NoOfVBitStreams());
 	this->_noOfUniqueVals = _uniqueValList.size();
 
 	MultiCatDataInfo *df = new MultiCatDataInfo(_uniqueValList);
 	this->_mappedIntVals = df->getAssignedEncodedNumberList();
 	
-	for (int j = 0 ; j < this->_noOfVBitStreams ; j++)
+	for (int j = 0 ; j < this->NoOfVBitStreams() ; j++)
 	{
-		this->_vBitStreamSet[j] = new VBitStream(noOfRows);
+		this->vBitStreams()[j] = new VBitStream(noOfRows);
 	}
 
 	for (int i = 0 ; i < noOfRows ; i++)
 	{
 		int pos = std::find(_uniqueValList.begin(),_uniqueValList.end(),_valueList[i]) - _uniqueValList.begin();
-		dynamic_bitset<> bitSet(this->_noOfVBitStreams,(unsigned long)pos);
+		dynamic_bitset<> bitSet(this->NoOfVBitStreams(),(unsigned long)pos);
 		this->_mappedValList.push_back(bitSet);
 	}
 
@@ -48,69 +49,19 @@ vector<dynamic_bitset<>> EncodedMultiCatAttribute::mappedValList(){
 	return this->_mappedValList;
 }
 
-int EncodedMultiCatAttribute::NoOfVBitStreams(){
-	return this->_noOfVBitStreams;
-}
-
-vector<VBitStream*> EncodedMultiCatAttribute::vBitStreams(){
-	return this->_vBitStreamSet;
-}
-
-string EncodedMultiCatAttribute::attributeName(){
-	return this->_attName;
-}
-
-int EncodedMultiCatAttribute::attributeID(){
-	return this->_attID;
-}
-
-ATT_TYPE EncodedMultiCatAttribute::attributeType(){
-	return this->_attType;
-}
-
 vector<string> EncodedMultiCatAttribute::uniqueValList(){
 	return this->_uniqueValList;
 }
 
-void EncodedMultiCatAttribute::setAttID(int attID){
-	this->_attID = attID;
-}
-
-void EncodedMultiCatAttribute::setAttName(std::string name){
-	this->_attName = name;
-}
-
-void EncodedMultiCatAttribute::setAttType(ATT_TYPE type){
-	this->_attType = type;
-}
-
-void EncodedMultiCatAttribute::setNoOfVBitStreams(int novBitsets, int rows){
-	this->_noOfVBitStreams = novBitsets;
-	this->_vBitStreamSet.resize(this->_noOfVBitStreams);
-
-	for (int i = 0 ; i <novBitsets ; i++)
-	{
-		this->_vBitStreamSet[i] = new VBitStream(rows);
-	}
-}
-
-VBitStream* EncodedMultiCatAttribute::bitStreamAt(int bitStreamID){
-	return this->_vBitStreamSet[bitStreamID];
-}
-
-string EncodedMultiCatAttribute::DecodeTheTuple(int tupleID){
+string EncodedMultiCatAttribute::decodeTheTuple(int tupleID){
 	int i = 0;
-	bool* bitSet = new bool[this->_noOfVBitStreams];
+	bool* bitSet = new bool[this->NoOfVBitStreams()];
 	int val=0;
-	for (; i<this->_noOfVBitStreams ; i++)
+	for (; i<this->NoOfVBitStreams() ; i++)
 	{
-		bitSet[i] = this->_vBitStreamSet[i]->BitStream()[tupleID - 1];
-		if(bitSet[i]) {val += pow(2.0,(double)(this->_noOfVBitStreams - i - 1));}
+		bitSet[i] = this->vBitStreams()[i]->BitStream()[tupleID - 1];
+		if(bitSet[i]) {val += pow(2.0,(double)(this->NoOfVBitStreams() - i - 1));}
 	}
 	
 	return this->_uniqueValList[val];
-}
-
-VBitStream* EncodedMultiCatAttribute::operator()(const int bitStreamID){
-	return this->bitStreamAt(bitStreamID);
 }
--- trunk/Extraction/SMDataExtraction/SMDataExtraction/WrapDataSource.h
+++ trunk/Extraction/SMDataExtraction/SMDataExtraction/WrapDataSource.h
@@ -3,6 +3,7 @@
 #include "EncodedIntAttribute.h"
 #include "EncodedMultiCatAttribute.h"
 #include "DecodedTuple.h"
+#include "EncodedAttributeInfo.h"
 #include <vector>
 
 using namespace DBQueryExecutionInfo;
@@ -17,14 +18,17 @@ public:
 	__declspec(dllexport) void encodeAtrributes();
 	__declspec(dllexport) vector<EncodedIntAttribute*> codedIntAtts();
 	__declspec(dllexport) vector<EncodedMultiCatAttribute*> codedStringAtts();
+	__declspec(dllexport) vector<EncodedAttributeInfo*> codedAttributes();
 	__declspec(dllexport) Tuple* DecodeTheTuple(int tupleID);
+	__declspec(dllexport) EncodedAttributeInfo* operator()(const int attID);
+	__declspec(dllexport) VBitStream* operator()(const int attID,const int bitStreamID);
 	void encodeIntAttributes(vector<PureIntAttInfo*> intAtts);
 	void encodeStringAttributes(vector<PureStringAttInfo*> stringAtts);
-
-
+	
 
 private:
 	DBQueryExecution _queryDataInfo;
+	vector<EncodedAttributeInfo*> _codedAtts;
 	vector<EncodedIntAttribute*> _codedIntAtts;
 	vector<EncodedMultiCatAttribute*> _codedStringAtts;
 	int _noOfRows;
