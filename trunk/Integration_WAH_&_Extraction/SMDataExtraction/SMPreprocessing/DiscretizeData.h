37d706b10348a662fed9edb0b9dedac43b8f7ad4
samfernandopulle@gmail.com <samfernandopulle@gmail.com@c7f6ba40-6498-11de-987a-95e5a5a5d5f1>
2010-05-04 08:03:40 +0000

    add some discritize additional functions and fix the issue in NULL removal

--- trunk/Integration_WAH_&_Extraction/SMDataExtraction/SMPreprocessing/DiscretizeData.h
+++ trunk/Integration_WAH_&_Extraction/SMDataExtraction/SMPreprocessing/DiscretizeData.h
@@ -1,16 +1,20 @@
 #include "EncodedDoubleAttribute.h"
 #include "EncodedIntAttribute.h"
+#include "WrapDataSource.h"
 #pragma once
 
 class DiscretizeData
 {
 public:
 	__declspec(dllexport) DiscretizeData(void);
-	__declspec(dllexport) DiscretizeData(EncodedDoubleAttribute* doubleAtt,long rowCount){this->_ctsAtt = doubleAtt;this->_rowCount = rowCount;}
-	__declspec(dllexport) EncodedIntAttribute* DiscretizeCtsAttribute();
+	__declspec(dllexport) DiscretizeData(long rowCount){this->_rowCount = rowCount;}
+	__declspec(dllexport) DiscretizeData(WrapDataSource *ds){this->_ctsDs = ds;this->_rowCount = ds->noOfRows();}
+	__declspec(dllexport) EncodedIntAttribute* DiscretizeCtsAttribute(EncodedDoubleAttribute* _ctsAtt);
+	__declspec(dllexport) void DiscretizeAllCtsAttributes();
+	__declspec(dllexport) void DiscretizeSelectedCtsAtts(vector<int> zeroBasedAttIDs);
 	__declspec(dllexport) ~DiscretizeData(void);
 
 private:
-	EncodedDoubleAttribute* _ctsAtt;
+	WrapDataSource* _ctsDs;
 	long _rowCount;
 };
