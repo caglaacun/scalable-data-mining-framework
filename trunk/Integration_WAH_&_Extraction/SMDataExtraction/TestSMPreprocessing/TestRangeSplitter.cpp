d47f9e587e91592089c800cc6021365e8650dfc9
samfernandopulle@gmail.com <samfernandopulle@gmail.com@c7f6ba40-6498-11de-987a-95e5a5a5d5f1>
2010-05-08 04:08:47 +0000

    add an example to new range splitting method

--- trunk/Integration_WAH_&_Extraction/SMDataExtraction/TestSMPreprocessing/TestRangeSplitter.cpp
+++ trunk/Integration_WAH_&_Extraction/SMDataExtraction/TestSMPreprocessing/TestRangeSplitter.cpp
@@ -1,5 +1,6 @@
 #include "TestRangeSplitter.h"
-#include "..\..\..\..\..\mydocuments\visual studio 2008\projects\smdataextraction\smpreprocessing\rangesplitter.h"
+//#include "..\..\..\..\..\mydocuments\visual studio 2008\projects\smdataextraction\smpreprocessing\rangesplitter.h"
+#include "..\SMPreprocessing\RangeSplitter.h"
 #include "..\SMDataExtraction\LoadSavedDataSources.h"
 #include "..\SMDataExtraction\DataSources.h"
 
@@ -19,39 +20,59 @@ void TestRangeSplitter::TestSuite()
 
 void TestRangeSplitter::UniqueBitmapCreation()
 {
-	TestUniqueBitmapCreation(CreateDataSource("intVal_single_data","intVal_single_metadata","intVal_100000"));
+	TestUniqueBitmapCreation(CreateDataSource("splitter_test_data","splitter_test_metadata","small_splitter_test"));
 }
 
 void TestRangeSplitter::TestUniqueBitmapCreation(WrapDataSource * _source)
 {
-	//Ranges considered [0,1],(1,2],(2,3]
-EncodedAttributeInfo * inf = (*_source)(0);
-RangeSplitter splitter;
-vector<BitStreamInfo * > unique_maps = splitter.Splitter(inf,_source->noOfRows());
-
-
-vector<int> vect(_source->noOfRows());
-CreateIntArray(vect,inf);
-int first_range = 0,second_range = 0, third_range = 0;
-for (size_t i = 0 ; i < vect.size() ; i++)
-{
-	if (vect[i] <= 1)
+	EncodedAttributeInfo * inf = (*_source)(0);
+	vector<double> vals(5);
+	vals[0] = 0;
+	vals[1] = 1;
+	vals[2] = 2;
+	vals[3] = 3;
+	vals[4] = 4;
+	RangeSplitter *splitter = new RangeSplitter(inf,_source->noOfRows());
+	EncodedMultiCatAttribute* mulAtt = splitter->SplitIntoEqualRanges(3);
+	cout<<"Unique Values : "<<endl;
+	for (int i = 0 ; i < mulAtt->uniqueValList().size() ; i++)
 	{
-		first_range++;
+		cout << mulAtt->uniqueValList()[i]<<endl;
 	}
-	else if (vect[i] <= 2 && vect[i] > 1)
+	cout<<"Converted Values : "<<endl;
+	for (int j = 0 ; j < _source->noOfRows() ; j++)
 	{
-		second_range++;
+		cout << mulAtt->decodeTheTuple(j + 1)<<endl;
 	}
-	else {
-		third_range++;
-	}
-}
 
-//Testing whether the counts obtain from both methods are equal
-cout << "Unique Map 0 " << unique_maps[0]->count() << " " << first_range << endl;
-cout << "Unique Map 1 " << unique_maps[1]->count() << " " << second_range << endl;
-cout << "Unique Map 2 " << unique_maps[2]->count() << " " << third_range << endl;
+	////Ranges considered [0,1],(1,2],(2,3]
+	//EncodedAttributeInfo * inf = (*_source)(0);
+	//RangeSplitter splitter;
+	//vector<BitStreamInfo * > unique_maps = splitter.Splitter(inf,_source->noOfRows());
+
+
+	//vector<int> vect(_source->noOfRows());
+	//CreateIntArray(vect,inf);
+	//int first_range = 0,second_range = 0, third_range = 0;
+	//for (size_t i = 0 ; i < vect.size() ; i++)
+	//{
+	//	if (vect[i] <= 1)
+	//	{
+	//		first_range++;
+	//	}
+	//	else if (vect[i] <= 2 && vect[i] > 1)
+	//	{
+	//		second_range++;
+	//	}
+	//	else {
+	//		third_range++;
+	//	}
+	//}
+
+	////Testing whether the counts obtain from both methods are equal
+	//cout << "Unique Map 0 " << unique_maps[0]->count() << " " << first_range << endl;
+	//cout << "Unique Map 1 " << unique_maps[1]->count() << " " << second_range << endl;
+	//cout << "Unique Map 2 " << unique_maps[2]->count() << " " << third_range << endl;
 }
 
 void TestRangeSplitter::CreateIntArray(vector<int> & _vect,EncodedAttributeInfo * _attr)
@@ -76,9 +97,9 @@ void TestRangeSplitter::CreateIntArray(vector<int> & _vect,EncodedAttributeInfo 
 
 }
 
-WrapDataSource * TestRangeSplitter::CreateDataSource(string datafile,string metadFile,string filename)
+WrapDataSource * TestRangeSplitter::CreateDataSource(string datafile,string metadFile,string dsnName)
 {
 	LoadSavedDataSources * m_lsd = new LoadSavedDataSources(metadFile,datafile);
 	DataSources * m_data_sources = m_lsd->loadSavedEncodedData();
-	return (*m_data_sources)(filename);
+	return (*m_data_sources)(dsnName);
 }
